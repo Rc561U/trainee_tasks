825fbc7e83c5c795bcc1817c6acc390eade4b1a3
Rc561U <hugeanton29011994@gmail.com>
2022-10-04 16:21:26 +0300

    refactor: code formating

--- Task_2.php
+++ Task_2.php
@@ -2,31 +2,30 @@
 class Birthday
 {
 
-    private $nextdate;
-    private $tonext;
-    private $result;
-    public function __construct($date)
-    {
-        if ($this->validateInputFormat($date) && $this->is_date_exist($date)) {
+	private $nextdate;
+	private $tonext;
+	private $result;
+	public function __construct($date)
+	{
+		if ($this->validateInputFormat($date) && $this->is_date_exist($date)) {
 			$birthdate = new DateTime($date);
-            $today = new DateTime("today");
-            $this->nextdate = new DateTime($birthdate->format('d F') . " this year");
-            $this->nextdate >= $today or $this->nextdate->modify("+1 year");
-            $this->tonext = $today->diff($this->nextdate);
-            $this->result = $this->tonext->format('%a'). ' days are left until the entered birthday';
+			$today = new DateTime("today");
+			$this->nextdate = new DateTime($birthdate->format('d F') . " this year");
+			$this->nextdate >= $today or $this->nextdate->modify("+1 year");
+			$this->tonext = $today->diff($this->nextdate);
+			$this->result = $this->tonext->format('%a') . ' days are left until the entered birthday';
 		} else {
 			$this->result = "Incorrect date entered. Acceptable date format is 'DD-MM-YYYY'";
 		}
-        
-    }
+	}
 
-   
-    public function __toString()
-    {
-        return $this->result;
-    }
 
-    private function is_date_exist(string $date): bool
+	public function __toString()
+	{
+		return $this->result;
+	}
+
+	private function is_date_exist(string $date): bool
 	{
 		$d = DateTime::createFromFormat($format = 'd-m-Y', $date);
 		return $d && $d->format($format) === $date;

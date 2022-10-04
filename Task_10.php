668c0b5389971ff410bb3091eefab7fa14c37ec4
Rc561U <hugeanton29011994@gmail.com>
2022-10-04 14:09:49 +0300

    :fix implement correct operation for float numbers

--- Task_10.php
+++ Task_10.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 class Calculator
 {
-    private int $number_1;
-    private int $number_2;
+    private int|float $number_1;
+    private int|float $number_2;
     private string|int|float $result;
 
-    public function __construct(int $num_1, int $num_2)
+    public function __construct(int|float $num_1, int|float $num_2)
     {
         $this->number_1 = $num_1;
         $this->number_2 = $num_2;
@@ -31,11 +31,10 @@ class Calculator
 
     public function divide(): Calculator
     {
-        if ($this->number_2 === 0)
-        {
+        if ($this->number_2 == 0) {
             $this->result = "You cannot divide by 0";
         }
-        $this->result = $this->number_1 / $this->number_2;
+        $this->result = round($this->number_1 / $this->number_2, 2);
         return $this;
     }
 
@@ -45,31 +44,33 @@ class Calculator
         return $this;
     }
 
-    public function addBy(int $input_number): float
+    public function addBy(int|float $input_number): float
     {
         return $this->result + $input_number;
     }
 
 
-    public function multiplyBy(int $input_number): float
+    public function multiplyBy(int|float $input_number): int|float
     {
         return $this->result * $input_number;
     }
 
-    public function divideBy(int $input_number): string|int|float
+    public function divideBy(int|float $input_number): string|int|float
     {
-        
-        if ($input_number === 0){return "You cannot divide by 0";}
-        return $this->result / $input_number;
+
+        if ($input_number === 0) {
+            return "You cannot divide by 0";
+        }
+        return round($this->result / $input_number, 2);
     }
 
-    public function substractBy(int $input_number): float
+    public function substractBy(int|float $input_number): int|float
     {
         return $this->result - $input_number;
     }
 }
 
-$mycalc = new Calculator(12, 6);
+$mycalc = new Calculator(4, 2);
 echo $mycalc->add();
 echo "\n";
 echo $mycalc->multiply();
@@ -86,4 +87,3 @@ echo "\n";
 echo $mycalc->add()->multiplyBy(9);
 echo "\n";
 echo $mycalc->add()->addBy(9);
-
